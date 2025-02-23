2a2cf661226dada3ed33fefd7b8ebb1f685bad9c
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2025-02-23 20:56:08 +0530

    server started

---
+++ profile-creator-backend/src/index.ts
@@ -0,0 +1,9 @@
+import express, { Application } from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app: Application = express()
+
+
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`server is running : ${PORT}`))
